125c48ae2c050047a2680c3304d2944af3f14e40
ChikhaliyaYash <chikhaliyayash899@gmail.com>
2022-09-26 23:48:05 +0530

    Done

--- business card/src/App.js
+++ business card/src/App.js
@@ -6,7 +6,7 @@ import Main from "./components/Main";
 import "../src/index.css";
 
 export default function App() {
-  // return <h1>Hello World! Welcome to my small project</h1>;
+  // return <h1>Hello World! Welcome to my small project</h1>;hkjhkh
   return (
     <div className="container">
     <Card />
